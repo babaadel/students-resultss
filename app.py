3ca69d482fd995be0aa8c5ca416111e7fe00caa9
babaadel <babameine@gmail.com>
2026-05-19 15:29:57 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from fpdf2 import FPDF
+from fpdf import FPDF
 
 # 1. إعدادات الصفحة
 st.set_page_config(page_title="منصة التميز للنتائج", page_icon="🎓", layout="centered")
